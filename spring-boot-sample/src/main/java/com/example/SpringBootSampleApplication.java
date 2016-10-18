6948720ab0f868a73a7b09e643cea819b7ec4d13
Lalitanand Dandge <dandgelb@gmail.com>
2016-10-18 15:23:40 +0530

    Web Controller
    Ref: http://projects.spring.io/spring-boot/#quick-start
    spring-boot-starter-web added in gradle dependency

--- spring-boot-sample/src/main/java/com/example/SpringBootSampleApplication.java
+++ spring-boot-sample/src/main/java/com/example/SpringBootSampleApplication.java
@@ -1,13 +1,8 @@
 package com.example;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootSampleApplication {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
-		SpringApplication.run(SpringBootSampleApplication.class, args);
-	}
 }

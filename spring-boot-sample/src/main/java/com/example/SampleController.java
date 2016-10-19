b2ac3d96562051388690da6134c998cef74e6da6
Lalitanand Dandge <dandgelb@gmail.com>
2016-10-19 16:07:09 +0530

    Thymeleaf template engine
    RequestMapping for index.html
    Build dependency

--- spring-boot-sample/src/main/java/com/example/SampleController.java
+++ spring-boot-sample/src/main/java/com/example/SampleController.java
@@ -3,7 +3,9 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,11 +13,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SampleController {
 	@RequestMapping
 	@ResponseBody
-	
 	String home() {
 		return "Hello World!";
 	}
-	
+
+	@RequestMapping("/")
+	public String helloWorld(
+			@RequestParam(value = "username", required = false, defaultValue = "World") String username, Model model) {
+		model.addAttribute("username", username);
+		return "index";
+	}
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(SampleController.class, args);
 	}
